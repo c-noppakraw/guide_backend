6dbb4349c0b1b32531da26dc07f60ef821f8a051
drivera <c.noppakraw@gmail.com>
2020-04-01 20:12:16 +0700

    add list guide

--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,9 @@ const router = express.Router();
 
 /* GET home page. */
 router.use('/', require('./authentication'));
+router.use('/guide', require('./guide'));
+// router.get('/', (req, res) => {
+// 	res.render('index', { title: 'Express' });
+// });
 
 module.exports = router;
