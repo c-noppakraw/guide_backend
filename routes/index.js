5c74aa14627bcf2847affafd1f31e095410b7809
drivera <c.noppakraw@gmail.com>
2020-04-08 03:01:33 +0700

    add router

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,9 @@ const router = express.Router();
 /* GET home page. */
 router.use('/', require('./authentication'));
 router.use('/guide', require('./guide'));
+router.use('/tours', require('./tours'));
+router.use('/complain', require('./complain'));
+router.use('/question', require('./question'));
 // router.get('/', (req, res) => {
 // 	res.render('index', { title: 'Express' });
 // });
