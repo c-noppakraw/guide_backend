d40c4ee553cb2db66c9715b840f181e429c45b00
drivera <c.noppakraw@gmail.com>
2020-04-20 08:33:33 +0700

    add fn

--- routes/guide.js
+++ routes/guide.js
@@ -21,10 +21,19 @@ router.get('/', authMiddleware, async (req, res) => {
 			return res.redirect('/');
 		}
 		const user = await User.findAll({
-			where: { [Op.and]: [{ level: 4 }] },
+			where: { [Op.and]: [{ level: 4 }, { visible: '1' }] },
 			include: [{ model: Profile, as: 'Profile' }]
 		});
-		return res.status(201).json({ data_user: user });
+		return res.render('./guide/list_guide', { 
+			title: 'List Guide',
+			data: user,
+			img_profile: _img,
+			id_user: _id,
+            show_username: _username,
+            firstname_th: _firstname_th,
+            lastname_th: _lastname_th
+        });
+		// return res.status(201).json({ data_user: user });
 		// return res.render('index', { title: 'Guide Page' });
 	} catch (err) {
 		console.log(err);
@@ -32,7 +41,7 @@ router.get('/', authMiddleware, async (req, res) => {
 	}
 });
 
-router.get('/:guidesId', authMiddleware, async (req, res) => {
+router.get('/list', authMiddleware, async (req, res) => {
 	try {
 		const {
 			_id,
@@ -47,15 +56,154 @@ router.get('/:guidesId', authMiddleware, async (req, res) => {
 		if (_level == 4) {
 			return res.redirect('/');
 		}
-		const uuid = req.params.guidesId;
-		let user = {};
-		if (uuid) {
-			user = await User.findOne({
-				where: { [Op.and]: [ { level: 4 }, { uuid_user: uuid }] },
-				include: [{ model: Profile, as: 'Profile' }]
-			});
+		const user = await User.findAll({
+			where: { [Op.and]: [{ level: 4 }, { visible: '2' }] },
+			include: [{ model: Profile, as: 'Profile' }]
+		});
+		return res.render('./guide/confirm', { 
+			title: 'Confirm Guide',
+			data: user,
+			img_profile: _img,
+			id_user: _id,
+            show_username: _username,
+            firstname_th: _firstname_th,
+            lastname_th: _lastname_th
+        });
+		// return res.status(201).json({ data_user: user });
+		// return res.render('index', { title: 'Guide Page' });
+	} catch (err) {
+		console.log(err);
+		return res.status(500).json(err);
+	}
+});
+
+router.post('/:id_user/confirm', authMiddleware, async (req, res) => {
+	try {
+		const {
+            _id,
+            _level,
+            _username,
+            _firstname_th,
+            _firstname_en,
+            _lastname_th,
+            _lastname_en,
+            _img
+        } = req.user;
+        if (_level == 4) {
+            return res.redirect('/');
 		}
-		return res.status(201).json({ data_user: user });
+		const uuid = req.params.id_user;
+		let { visible } = req.body;
+		const newData = await User.update(
+			{ visible: visible },
+			{ where: { uuid_user: uuid } }
+		)
+		const newProfile = await Profile.update(
+			{ user_update: _username },
+			{ where: { user_uuid: uuid } }
+		)
+		// return res.status(201).json({ data_user: newData, data_profile: newProfile });
+		return res.redirect('/guide/list');
+	} catch (err) {
+		console.log(err);
+		return res.status(500).json(err);
+	}
+});
+
+router.get('/:id_user/detail', authMiddleware, async (req, res) => {
+    try {
+        const {
+            _id,
+            _level,
+            _username,
+            _firstname_th,
+            _firstname_en,
+            _lastname_th,
+            _lastname_en,
+            _img
+        } = req.user;
+        if (_level == 4) {
+            return res.redirect('/');
+		}
+		const uuid = req.params.id_user;
+        const user = await User.findOne({
+			where: { [Op.and]: [{ uuid_user: uuid }, { visible: '2' }] },
+			include: [{ model: Profile, as: 'Profile' }]
+		});
+		// return res.status(201).json({ data: user });
+		return res.render('./guide/daetail_confirm', { 
+			title: 'Confirm Guide',
+			data: user,
+			img_profile: _img,
+			id_user: _id,
+            show_username: _username,
+            firstname_th: _firstname_th,
+            lastname_th: _lastname_th
+        });
+    } catch (err) {
+        console.log(err);
+		return res.status(500).json(err);
+    }
+});
+
+router.get('/:id_user/edit', authMiddleware, async (req, res) => {
+	try {
+		const {
+			_id,
+			_level,
+			_username,
+			_firstname_th,
+			_firstname_en,
+			_lastname_th,
+			_lastname_en,
+			_img
+		} = req.user;
+		if (_level == 4) {
+			return res.redirect('/');
+		}
+		const uuid = req.params.id_user;
+        const user = await User.findOne({
+			where: { uuid_user: uuid },
+			include: [{ model: Profile, as: 'Profile' }]
+		});
+		// return res.status(201).json({ data_user: user });
+		return res.render('./guide/edit', { 
+			title: 'Edit Guide',
+			data: user,
+			img_profile: _img,
+			id_user: _id,
+            show_username: _username,
+            firstname_th: _firstname_th,
+            lastname_th: _lastname_th
+        });
+	} catch (err) {
+		console.log(err);
+		return res.status(500).json(err);
+	}
+});
+
+router.post('/:id_user/edit', authMiddleware, async (req, res) => {
+	try {
+		const {
+			_id,
+			_level,
+			_username,
+			_firstname_th,
+			_firstname_en,
+			_lastname_th,
+			_lastname_en,
+			_img
+		} = req.user;
+		if (_level == 4) {
+			return res.redirect('/');
+		}
+		const uuid = req.params.id_user;
+		console.log(req.body);
+        // const user = await User.findOne({
+		// 	where: { uuid_user: uuid },
+		// 	include: [{ model: Profile, as: 'Profile' }]
+		// });
+		// return res.status(201).json({ data_user: user });
 	} catch (err) {
 		console.log(err);
 		return res.status(500).json(err);
