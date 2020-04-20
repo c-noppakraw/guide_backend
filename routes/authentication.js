7e425e608715d07de2f7fc6da7048b0decb5098f
drivera <c.noppakraw@gmail.com>
2020-04-20 08:34:22 +0700

    comment validator

--- routes/authentication.js
+++ routes/authentication.js
@@ -18,47 +18,51 @@ router.post('/', login_validator, async (req, res) => {
 			username,
 			password
 		} = req.body;
-		const errors = validationResult(req);
-			if (!errors.isEmpty()) {
-				// return res.status(400).json({ errors: errors.array() });
-				return res.render('./login/login', { 
-					errors: errors.array(),
-					username
-				});
-			}
+		// const errors = validationResult(req);
+		// 	if (!errors.isEmpty()) {
+		// 		// return res.status(400).json({ errors: errors.array() });
+		// 		return res.render('./login/login', { 
+		// 			errors: errors.array(),
+		// 			username
+		// 		});
+		// 	}
 		const user = await User.findOne({
 			where: { username }
 		});
 		if(!user){
 			// return res.status(404).json({ errors: [{ msg: 'ไม่พบชื่อผู้ใช้' }] });
-			return res.render('./login/login', { errors: [{ msg: 'ไม่พบชื่อผู้ใช้' }] });
+			return res.render('./login/login', { errors_login: [{ msg: 'ไม่พบชื่อผู้ใช้' }] });
 		} else {
 			const user_uuid = user.uuid_user;
 			const profile = await Profile.findOne({
 				where: { user_uuid }
 			});
-
 			const isMatch = await bcrypt.compare(password, user.password);
 			if (!isMatch) {
 				// return res.status(400).json({ errors: [{ msg: 'รหัสผ่านไม่ถูกต้อง' }] });
-				return res.render('./login/login', { errors: [{ msg: 'รหัสผ่านไม่ถูกต้อง' }] });
+				return res.render('./login/login', { errors_login: [{ msg: 'รหัสผ่านไม่ถูกต้อง' }] });
 			}
-			const payload = {
-				_id: user.uuid_user,
-				_level: user.level,
-				_username: user.username,
-				_firstname_th: profile.firstname_th,
-				_firstname_en: profile.firstname_en,
-				_lastname_th: profile.lastname_th,
-				_lastname_en: profile.lastname_en,
-				_img: profile.img
-			};
-			jwt.sign(payload, `secret_key_here`, { expiresIn: 3600 * 3 }, (err, token) => {
-				if (err) throw err;
-				res.cookie('authToken', token, { maxAge: 3600 * 3 * 1000, httpOnly: true });
-				return res.send('Login Success');
-				// res.redirect('/dashboards');
-			});
+			if (user.visible == 1) {
+				const payload = {
+					_id: user.uuid_user,
+					_level: user.level,
+					_username: user.username,
+					_firstname_th: profile.first_name_th,
+					_firstname_en: profile.first_name_en,
+					_lastname_th: profile.last_name_th,
+					_lastname_en: profile.last_name_en,
+					_img: user.img
+				};
+				jwt.sign(payload, `secret_key_here`, { expiresIn: 3600 * 3 }, (err, token) => {
+					if (err) throw err;
+					res.cookie('authToken', token, { maxAge: 3600 * 3 * 1000, httpOnly: true });
+					// return res.send('Login Success');
+					return res.redirect('/home');
+				});
+			} else {
+				// return res.send('Wait Confirm');
+				return res.redirect('/status/' + user.uuid_user);
+			}
 		}
 	} catch (err) {
 		console.log(err);
@@ -68,17 +72,16 @@ router.post('/', login_validator, async (req, res) => {
 
 router.get('/register', (req, res) => {
 	// return res.send('Register Page!');
-	return res.render('index', { title: 'Register Page' });
+	// return res.render('index', { title: 'Register Page' });
+	return res.render('./register/register', { title: 'Register Guide Go Inter' });
 });
 
 router.post('/register', uploadProfile.single('profile_image'), register_validator, async (req, res) => {
 	try {
 		let {
-			username,
-			password,
-			confirmPassword,
-			visible,
-			level,
+			user_name,
+			pass_word,
+			confirm_password,
 			first_name_th,
 			last_name_th,
 			first_name_en,
@@ -86,16 +89,39 @@ router.post('/register', uploadProfile.single('profile_image'), register_validat
 			code,
 			passport_no,
 			passport_exp,
-			country,
-			city,
+			frm_country,
+			frm_city,
 			phone,
-			email,
+			frm_email,
+			visa_no,
+			visa_exp,
+			visible,
+			level,
 			user_created,
 			user_update
 		} = req.body;
 		const errors = validationResult(req);
 			if (!errors.isEmpty()) {
-				return res.status(400).json({ errors: errors.array() });
+				// return res.status(400).json({ errors: errors.array() });
+				return res.render('./register/register', { 
+					errors: errors.array(),
+					user_name,
+					pass_word,
+					confirm_password,
+					first_name_th,
+					last_name_th,
+					first_name_en,
+					last_name_en,
+					code,
+					passport_no,
+					passport_exp,
+					frm_country,
+					frm_city,
+					phone,
+					frm_email,
+					visa_no,
+					visa_exp
+				});
 			}
 		let profileImage = '';
 			if (req.file) {
@@ -104,9 +130,9 @@ router.post('/register', uploadProfile.single('profile_image'), register_validat
 				profileImage = 'no-image.png';
 			}
 		const salt = await bcrypt.genSalt(10);
-		password = await bcrypt.hash(password, salt);
+		password = await bcrypt.hash(pass_word, salt);
 		const newUser = await User.create({ 
-			username, 
+			username: user_name, 
 			password,
 			visible, 
 			level,
@@ -120,16 +146,20 @@ router.post('/register', uploadProfile.single('profile_image'), register_validat
 			passport_no,
 			passport_exp,
 			code,
-			country,
-			city,
+			country: frm_country,
+			city: frm_city,
 			phone,
-			email,
+			visa_no,
+			visa_exp,
+			email: frm_email,
 			user_created,
 			user_update,
 			ProfileUuidUser: newUser.uuid_user
 		});
-		return res.status(201).json({ data_user: newUser, data_profile: newProfile });
-		// return res.status(201).json({ data_user: newUser });
+		// return res.status(201).json({ data_user: newUser, data_profile: newProfile });
+		return res.render('./login/login', {
+			alerts: [{ status: 'success', msg: 'สมัครสมาชิกเรียบร้อย' }]
+		});
 	} catch (err) {
 		console.log(err);
 		return res.status(500).json(err);
